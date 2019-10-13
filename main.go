76a494e987b9c9fc16fbc5a772bff781c920ae03
Adam <sector-f@users.noreply.github.com>
2019-10-13 19:16:15 -0400

    got basic functionality working

--- main.go
+++ main.go
@@ -18,8 +18,26 @@ func main() {
 			continue
 		}
 
-		if !(fileInfo.Mode()&os.ModeSymlink != 0) {
+		if fileInfo.Mode()&os.ModeSymlink == 0 {
 			fmt.Fprintf(os.Stderr, "%v is not a symlink; skipping\n", filename)
+			continue
+		}
+
+		realName, err := os.Readlink(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error calling readlink on %v: %v\n", filename, err)
+			continue
+		}
+
+		err = os.Remove(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error deleting %v: %v\n", filename, err)
+			continue
+		}
+
+		err = os.Remove(realName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error deleting %v: %v\n", realName, err)
 			continue
 		}
 	}
